38f53fdecc1d633455ebda01ffc81205d29f6e5e
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-10-01 22:13:11 +0800

    Add codes

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,32 +1,76 @@
-#include "mod/MyMod.h"
+#include "MyMod.h"
 
+#include "ll/api/memory/Hook.h"
 #include "ll/api/mod/RegisterHelper.h"
+#include "mc/world/level/BlockSource.h"
+#include "mc/world/level/BlockPos.h"
+#include "mc/world/level/block/Block.h"
+#include "mc/world/level/block/FireBlock.h"
+#include "mc/world/level/Level.h" 
 
-namespace my_mod {
+namespace infinite_fire {
 
-MyMod& MyMod::getInstance() {
-    static MyMod instance;
+// 阻止火焰被扑灭
+LL_TYPE_INSTANCE_HOOK(
+    LevelExtinguishFireHook,
+    ll::memory::HookPriority::Highest,
+    Level,
+    &Level::$extinguishFire,
+    bool,
+    [[maybe_unused]] BlockSource&     region,
+    [[maybe_unused]] BlockPos const&  pos,
+    [[maybe_unused]] unsigned char    face,
+    [[maybe_unused]] Actor* source
+) {
+    return false;
+}
+
+// 加速火焰的更新和蔓延
+LL_TYPE_INSTANCE_HOOK(
+    FireBlockNeighborChangedHook,
+    ll::memory::HookPriority::Normal,
+    FireBlock,
+    &FireBlock::$neighborChanged,
+    void,
+    BlockSource&     region,
+    BlockPos const&  pos,
+    BlockPos const&  neighborPos
+) {
+    origin(region, pos, neighborPos);
+
+    auto const& block = region.getBlock(pos);
+    if (block.isAir()) {
+        return;
+    }
+    region.addToTickingQueue(pos, block, 1, 0, false);
+}
+
+struct HookImpl {
+    ll::memory::HookRegistrar<
+        LevelExtinguishFireHook,
+        FireBlockNeighborChangedHook
+    > mHookRegistrar;
+};
+
+InfiniteFireMod& InfiniteFireMod::getInstance() {
+    static InfiniteFireMod instance;
     return instance;
 }
 
-bool MyMod::load() {
-    getSelf().getLogger().debug("Loading...");
-    // Code for loading the mod goes here.
+bool InfiniteFireMod::load() {
+    return true; 
+}
+
+bool InfiniteFireMod::enable() {
+    mHookImpl = std::make_unique<HookImpl>();
     return true;
 }
 
-bool MyMod::enable() {
-    getSelf().getLogger().debug("Enabling...");
-    // Code for enabling the mod goes here.
+bool InfiniteFireMod::disable() {
+    mHookImpl.reset();
     return true;
 }
 
-bool MyMod::disable() {
-    getSelf().getLogger().debug("Disabling...");
-    // Code for disabling the mod goes here.
-    return true;
-}
+} // namespace infinite_fire
 
-} // namespace my_mod
-
-LL_REGISTER_MOD(my_mod::MyMod, my_mod::MyMod::getInstance());
+LL_REGISTER_MOD(infinite_fire::InfiniteFireMod, infinite_fire::InfiniteFireMod::getInstance());
--- src/mod/MyMod.h
+++ src/mod/MyMod.h
@@ -1,33 +1,24 @@
 #pragma once
 
-#include "ll/api/mod/NativeMod.h"
+#include <memory>
 
-namespace my_mod {
+namespace infinite_fire {
 
-class MyMod {
+struct HookImpl;
+
+class InfiniteFireMod {
 
 public:
-    static MyMod& getInstance();
+    static InfiniteFireMod& getInstance();
 
-    MyMod() : mSelf(*ll::mod::NativeMod::current()) {}
+    InfiniteFireMod() = default;
 
-    [[nodiscard]] ll::mod::NativeMod& getSelf() const { return mSelf; }
-
-    /// @return True if the mod is loaded successfully.
     bool load();
-
-    /// @return True if the mod is enabled successfully.
     bool enable();
-
-    /// @return True if the mod is disabled successfully.
     bool disable();
 
-    // TODO: Implement this method if you need to unload the mod.
-    // /// @return True if the mod is unloaded successfully.
-    // bool unload();
-
 private:
-    ll::mod::NativeMod& mSelf;
+    std::unique_ptr<HookImpl> mHookImpl;
 };
 
-} // namespace my_mod
+} // namespace infinite_fire
