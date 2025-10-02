c303c7fb6ffe3847ad112a0a554739695a0d71b9
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-10-03 00:24:14 +0800

    Fix

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,56 +1,118 @@
 #include "MyMod.h"
 
+#include "ll/api/Config.h"
+#include "ll/api/io/Logger.h"
+#include "ll/api/chrono/GameChrono.h"
+#include "ll/api/coro/CoroTask.h"
+#include "ll/api/event/EventBus.h"
+#include "ll/api/event/world/BlockChangedEvent.h"
 #include "ll/api/memory/Hook.h"
 #include "ll/api/mod/RegisterHelper.h"
-#include "mc/world/level/BlockPos.h"
-#include "mc/world/level/BlockSource.h"
+#include "ll/api/service/Bedrock.h"
+#include "ll/api/thread/ServerThreadExecutor.h"
+#include "ll/api/utils/ErrorUtils.h"
+
+#include "mc/world/level/Level.h"
 #include "mc/world/level/block/Block.h"
 #include "mc/world/level/block/FireBlock.h"
+#include "mc/world/level/dimension/Dimension.h"
+#include "mc/world/level/BlockPos.h"
+
+#include <string_view>
+
+using namespace ll::literals::chrono_literals;
 
 namespace infinite_fire {
 
-LL_TYPE_INSTANCE_HOOK(
-    FireBlockNeighborChangedHook,
-    ll::memory::HookPriority::Normal,
-    FireBlock,
-    &FireBlock::$neighborChanged,
-    void,
-    BlockSource&     region,
-    BlockPos const&  pos,
-    BlockPos const&  neighborPos
-) {
-    origin(region, pos, neighborPos);
-
-    auto const& block = region.getBlock(pos);
-    if (block.isAir()) {
-        return;
-    }
-
-    constexpr int accelerationFactor = 5;
-    for (int i = 0; i < accelerationFactor; ++i) {
-        // void addToTickingQueue(BlockPos const&, Block const&, int tickDelay, int priorityOffset, bool skipOverrides);
-        region.addToTickingQueue(pos, block, 1, 0, false);
-    }
-}
+std::unordered_set<BlockPos> gFirePositions;
 
 LL_TYPE_INSTANCE_HOOK(
-    FireBlockMayPlaceHook,
+    LevelExtinguishFireHook,
     ll::memory::HookPriority::Highest,
-    FireBlock,
-    &FireBlock::$mayPlace,
+    Level,
+    &Level::$extinguishFire,
     bool,
-    [[maybe_unused]] BlockSource& region,
-    [[maybe_unused]] BlockPos const& pos
-) {
-    return true;
+    [[maybe_unused]] BlockSource&,
+    [[maybe_unused]] BlockPos const&,
+    [[maybe_unused]] unsigned char,
+    [[maybe_unused]] Actor*) {
+    return false;
 }
 
-struct HookImpl {
-    ll::memory::HookRegistrar<
-        FireBlockNeighborChangedHook,
-        FireBlockMayPlaceHook
-    > mHookRegistrar;
+struct Impl {
+    ll::memory::HookRegistrar<LevelExtinguishFireHook> mHookRegistrar;
+
+    ll::event::ListenerPtr mBlockChangeListener;
+    std::shared_ptr<bool>  mIsTaskRunning = std::make_shared<bool>(false);
+
+    ll::coro::CoroTask<> startUpdateTask() const {
+        *mIsTaskRunning = true;
+        while (*mIsTaskRunning) {
+            if (!InfiniteFireMod::getInstance().getConfig().isSpreadEnabled) {
+                co_await 20_tick;
+                continue;
+            }
+
+            co_await 10_tick;
+
+            auto level = ll::service::getLevel();
+            if (!level) continue;
+
+            auto dimension = level->getDimension(0).lock();
+            if (!dimension) continue;
+
+            auto& const_region = dimension->getBlockSourceFromMainChunkSource();
+            auto& region       = const_cast<BlockSource&>(const_region);
+
+            auto fireBlock = Block::tryGetFromRegistry(std::string_view{"minecraft:fire"});
+            if (!fireBlock) continue;
+
+            auto& fireBlockType = static_cast<FireBlock const&>(fireBlock->getBlockType());
+
+            std::vector<BlockPos> newFires;
+
+            for (const auto& pos : gFirePositions) {
+                for (int x = -1; x <= 1; ++x) {
+                    for (int y = -1; y <= 1; ++y) {
+                        for (int z = -1; z <= 1; ++z) {
+                            if (x == 0 && y == 0 && z == 0) continue;
+                            BlockPos neighborPos = pos + BlockPos{x, y, z};
+                            if (region.getBlock(neighborPos).isAir() && fireBlockType.getFireOdds(region, neighborPos) > 0) {
+                                newFires.push_back(neighborPos);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (newFires.empty()) continue;
+
+            for (const auto& newPos : newFires) {
+                region.setBlock(newPos, *fireBlock, 3, nullptr, nullptr);
+            }
+        }
+        co_return;
+    }
+
+    Impl() {
+        mBlockChangeListener = ll::event::EventBus::getInstance().emplaceListener<ll::event::BlockChangedEvent>(
+            [](ll::event::BlockChangedEvent& event) {
+                if (event.newBlock().getTypeName() == "minecraft:fire") {
+                    gFirePositions.insert(event.pos());
+                } else if (event.previousBlock().getTypeName() == "minecraft:fire") {
+                    gFirePositions.erase(event.pos());
+                }
+            });
+
+        startUpdateTask().launch(ll::thread::ServerThreadExecutor::getDefault());
+    }
+
+    ~Impl() {
+        ll::event::EventBus::getInstance().removeListener(mBlockChangeListener);
+        *mIsTaskRunning = false;
+    }
 };
+
 
 InfiniteFireMod& InfiniteFireMod::getInstance() {
     static InfiniteFireMod instance;
@@ -58,18 +120,34 @@ InfiniteFireMod& InfiniteFireMod::getInstance() {
 }
 
 bool InfiniteFireMod::load() {
+    mConfig.emplace();
+    try {
+        ll::config::loadConfig(*mConfig, getSelf().getConfigDir() / "config.json");
+    } catch (...) {
+        ll::error_utils::printCurrentException(getSelf().getLogger());
+    }
+    saveConfig();
+    
+    getSelf().getLogger().info("InfiniteFire loaded!");
     return true;
 }
 
 bool InfiniteFireMod::enable() {
-    mHookImpl = std::make_unique<HookImpl>();
+    mImpl = std::make_unique<Impl>();
+    getSelf().getLogger().info("InfiniteFire enabled!");
     return true;
 }
 
 bool InfiniteFireMod::disable() {
-    mHookImpl.reset();
+    gFirePositions.clear();
+    mImpl.reset();
+    getSelf().getLogger().info("InfiniteFire disabled!");
     return true;
 }
+
+Config& InfiniteFireMod::getConfig() { return mConfig.value(); }
+
+void InfiniteFireMod::saveConfig() { ll::config::saveConfig(*mConfig, getSelf().getConfigDir() / "config.json"); }
 
 } // namespace infinite_fire
 
--- src/mod/MyMod.h
+++ src/mod/MyMod.h
@@ -1,24 +1,43 @@
 #pragma once
 
+#include "mc/world/level/BlockPos.h"
+#include <ll/api/mod/NativeMod.h>
+
 #include <memory>
+#include <unordered_set>
+#include <optional>
 
 namespace infinite_fire {
 
-struct HookImpl;
+struct Impl;
+
+extern std::unordered_set<BlockPos> gFirePositions;
+
+struct Config {
+    bool isSpreadEnabled = false;
+    int  version         = 1;
+};
 
 class InfiniteFireMod {
-
 public:
     static InfiniteFireMod& getInstance();
 
-    InfiniteFireMod() = default;
+    InfiniteFireMod() : mSelf(*ll::mod::NativeMod::current()) {}
+
+    [[nodiscard]] ll::mod::NativeMod& getSelf() const { return mSelf; }
 
     bool load();
     bool enable();
     bool disable();
 
+    Config& getConfig();
+    void    saveConfig();
+
 private:
-    std::unique_ptr<HookImpl> mHookImpl;
+    ll::mod::NativeMod&   mSelf;
+    std::unique_ptr<Impl> mImpl;
+    
+    std::optional<Config> mConfig; 
 };
 
 } // namespace infinite_fire
